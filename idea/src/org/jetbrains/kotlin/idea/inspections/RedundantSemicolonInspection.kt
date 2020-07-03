iswadigunarso/kotlin

commit 741ebeb7b81355a68b03d2f20efeef479c01e222

Mark semicolons after nullable types redundant, despite of '!' after (KT-38240)

idea/src/org/jetbrains/kotlin/idea/inspections/RedundantSemicolonInspection.kt
@@ -86,20 +86,30 @@ class RedundantSemicolonInspection : AbstractKotlinInspection(), CleanupLocalIns
                 return false // case with statement starting with '{' and call on the previous line
             }
 
+            return !isSemicolonAllowed(semicolon)
+        }
+
+        private fun isSemicolonAllowed(semicolon: PsiElement): Boolean {
             if (isRequiredForCompanion(semicolon)) {
-                return false
+                return true
             }
 
             val prevSibling = semicolon.getPrevSiblingIgnoringWhitespaceAndComments()
             val nextSibling = semicolon.getNextSiblingIgnoringWhitespaceAndComments()
-            if (prevSibling.safeAs<KtNameReferenceExpression>()?.text in softModifierKeywords &&
-                nextSibling is KtDeclaration
-            ) return false
-            if (nextSibling.safeAs<KtPrefixExpression>()?.operationToken == KtTokens.EXCL &&
-                semicolon.prevLeaf()?.getStrictParentOfType<KtTypeReference>() != null
-            ) return false
 
-            return true
+            if (prevSibling.safeAs<KtNameReferenceExpression>()?.text in softModifierKeywords && nextSibling is KtDeclaration) {
+                // enum; class Foo
+                return false
+            }
+
+            if (nextSibling is KtPrefixExpression && nextSibling.operationToken == KtTokens.EXCL) {
+                val typeElement = semicolon.prevLeaf()?.getStrictParentOfType<KtTypeReference>()?.typeElement
+                if (typeElement != null) {
+                    return typeElement !is KtNullableType // trailing '?' fixes parsing
+                }
+            }
+
+            return false
         }
 
         private fun isRequiredForCompanion(semicolon: PsiElement): Boolean {

idea/testData/inspectionsLocal/redundantSemicolon/betweenNullableTypeAndNotOperator.kt
@@ -1,0 +1,4 @@
+fun test(a: Any) {
+    val b = a as Boolean?;<caret>
+    !b
+}

idea/testData/inspectionsLocal/redundantSemicolon/betweenNullableTypeAndNotOperator.kt.after
@@ -1,0 +1,4 @@
+fun test(a: Any) {
+    val b = a as Boolean?
+    !b
+}

idea/tests/org/jetbrains/kotlin/idea/inspections/LocalInspectionTestGenerated.java
@@ -8396,6 +8396,11 @@ public class LocalInspectionTestGenerated extends AbstractLocalInspectionTest {
             runTest("idea/testData/inspectionsLocal/redundantSemicolon/beforeKDocAndLambda.kt");
         }
 
+        @TestMetadata("betweenNullableTypeAndNotOperator.kt")
+        public void testBetweenNullableTypeAndNotOperator() throws Exception {
+            runTest("idea/testData/inspectionsLocal/redundantSemicolon/betweenNullableTypeAndNotOperator.kt");
+        }
+
         @TestMetadata("betweenSoftKeywordAndDeclaration.kt")
         public void testBetweenSoftKeywordAndDeclaration() throws Exception {
             runTest("idea/testData/inspectionsLocal/redundantSemicolon/betweenSoftKeywordAndDeclaration.kt");

commit 445c721b26f9da116dfa2dd1328006ea49699bb7

Fix redundant semicolon before soft keywords
 Fixes few tests, this bug was introduced in 741ebeb7b81355a68b03d2f20efeef479c01e222

idea/src/org/jetbrains/kotlin/idea/inspections/RedundantSemicolonInspection.kt
@@ -86,10 +86,10 @@ class RedundantSemicolonInspection : AbstractKotlinInspection(), CleanupLocalIns
                 return false // case with statement starting with '{' and call on the previous line
             }
 
-            return !isSemicolonAllowed(semicolon)
+            return !isSemicolonRequired(semicolon)
         }
 
-        private fun isSemicolonAllowed(semicolon: PsiElement): Boolean {
+        private fun isSemicolonRequired(semicolon: PsiElement): Boolean {
             if (isRequiredForCompanion(semicolon)) {
                 return true
             }
@@ -99,7 +99,7 @@ class RedundantSemicolonInspection : AbstractKotlinInspection(), CleanupLocalIns
 
             if (prevSibling.safeAs<KtNameReferenceExpression>()?.text in softModifierKeywords && nextSibling is KtDeclaration) {
                 // enum; class Foo
-                return false
+                return true
             }
 
             if (nextSibling is KtPrefixExpression && nextSibling.operationToken == KtTokens.EXCL) {